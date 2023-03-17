b7f0cfcca87af7b372c0e7efa51b48c370d1d260
Flyman <1308646+zhangmx@users.noreply.github.com>
2023-03-17 10:43:58 +0800

    try project

--- node_red_config/settings.js
+++ node_red_config/settings.js
@@ -393,7 +393,7 @@ module.exports = {
 
         projects: {
             /** To enable the Projects feature, set this value to true */
-            enabled: false,
+            enabled: true,
             workflow: {
                 /** Set the default projects workflow mode.
                  *  - manual - you must manually commit changes
